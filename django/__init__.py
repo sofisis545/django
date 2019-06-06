55e7683e6bed50dc21594486aa5010f26cc745ea
cesar <cesar57927@gmail.com>
2019-06-06 13:21:18 -0500

    update version

--- django/__init__.py
+++ django/__init__.py
@@ -1,6 +1,6 @@
 from django.utils.version import get_version
-
-VERSION = (3, 0, 0, 'alpha', 0)
+# real version 3.0.0 alfa this for avoid override install
+VERSION = (2, 2, 2, 'final', 0)
 
 __version__ = get_version(VERSION)
 
